ccb5a983dba65069078d0afb1e5bffde6583445b
Ziad-24 <20201700308@cis.asu.edu.eg>
2025-05-02 01:04:04 +0300

    Change and adjust redirect to download page

--- app/download-app/page.tsx
+++ app/download-app/page.tsx
@@ -1,21 +1,42 @@
 "use client";
-import { useEffect } from "react";
+
+import { useEffect, useState } from "react";
 import { websiteLinks } from "@/data/links";
+
 export default function DownloadPage() {
+  const [deviceType, setDeviceType] = useState<"android" | "ios" | "other">(
+    "other"
+  );
+
   useEffect(() => {
     const userAgent = typeof window !== "undefined" ? navigator.userAgent : "";
     const IOS_LINK = websiteLinks.appStoreUrl;
     const ANDROID_LINK = websiteLinks.googlePlayUrl;
-    if (/android/i.test(userAgent)) {
+
+    const isAndroid = /android/i.test(userAgent);
+    const isIOS = /iphone|ipad|ipod/i.test(userAgent);
+
+    if (isAndroid) {
+      setDeviceType("android");
       window.location.href = ANDROID_LINK;
-    } else if (/iphone|ipad|ipod/i.test(userAgent)) {
+    } else if (isIOS) {
+      setDeviceType("ios");
       window.location.href = IOS_LINK;
     } else {
+      setDeviceType("other");
       window.location.href = "https://meshwareg.com";
     }
   }, []);
 
   return (
-    <p className="text-center py-20 text-lg">Redirecting to the app store...</p>
+    <p className="text-center py-20 text-lg">
+      Redirecting to the{" "}
+      {deviceType === "android"
+        ? "Google Play"
+        : deviceType === "ios"
+        ? "App Store"
+        : "Home Page"}
+      ...
+    </p>
   );
 }
