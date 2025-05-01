211a91e43b399024984b76f9093564b597962779
Ziad-24 <20201700308@cis.asu.edu.eg>
2025-05-01 06:58:52 +0300

    Change website links

--- data/links.ts
+++ data/links.ts
@@ -1,4 +1,6 @@
 export const websiteLinks = {
-  appStoreUrl: "#",
-  googlePlayUrl: "#",
+  appStoreUrl:
+    "https://apps.apple.com/eg/app/meshwar-eg-safe-rides/id6741827534",
+  googlePlayUrl:
+    "https://play.google.com/store/apps/details?id=com.meshwareg.customer",
 };
